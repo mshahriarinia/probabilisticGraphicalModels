943efe690cc81f147d6e0aac42d2add8a5573b76
Morteza Shahriari Nia <info@mshahriarinia.com>
2014-07-07 13:21:46 -0400

    phenotypeGivenGenotypeMendelianFactor.m done

--- PGM_Programming_Assignment_2/phenotypeGivenGenotypeMendelianFactor.m
+++ PGM_Programming_Assignment_2/phenotypeGivenGenotypeMendelianFactor.m
@@ -12,15 +12,15 @@ function phenotypeFactor = phenotypeGivenGenotypeMendelianFactor(isDominant, gen
 % maps to heterozygous, and assignment 3 maps to homozygous recessive.  For
 % example, say that there is a gene with two alleles, dominant allele A and
 % recessive allele a.  Assignment 1 would map to AA, assignment 2 would
-% make to Aa, and assignment 3 would map to aa.  For the phenotypes, 
-% assignment 1 maps to having the physical trait, and assignment 2 maps to 
+% make to Aa, and assignment 3 would map to aa.  For the phenotypes,
+% assignment 1 maps to having the physical trait, and assignment 2 maps to
 % not having the physical trait.
 %
 % THE VARIABLE TO THE LEFT OF THE CONDITIONING BAR MUST BE THE FIRST
 % VARIABLE IN THE .var FIELD FOR GRADING PURPOSES
 %
 % Input:
-%   isDominant: 1 if the trait is caused by the dominant allele (trait 
+%   isDominant: 1 if the trait is caused by the dominant allele (trait
 %   would be caused by A in example above) and 0 if the trait is caused by
 %   the recessive allele (trait would be caused by a in the example above)
 %   genotypeVar: The variable number for the genotype variable (goes in the
@@ -29,19 +29,31 @@ function phenotypeFactor = phenotypeGivenGenotypeMendelianFactor(isDominant, gen
 %   the .var part of the factor)
 %
 % Output:
-%   phenotypeFactor: Factor denoting the probability of having each 
+%   phenotypeFactor: Factor denoting the probability of having each
 %   phenotype for each genotype
 
 phenotypeFactor = struct('var', [], 'card', [], 'val', []);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %INSERT YOUR CODE HERE
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Fill in phenotypeFactor.var.  This should be a 1-D row vector.
 % Fill in phenotypeFactor.card.  This should be a 1-D row vector.
+phenotypeFactor.var = [phenotypeVar genotypeVar];
+phenotypeFactor.card = [2 3];
 
 phenotypeFactor.val = zeros(1, prod(phenotypeFactor.card));
+
+phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [1 1], isDominant); %AA
+phenotypeFactor = SetValueOfAssignment( phenotypeFactor, [2 1],~isDominant); %AA
+phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [1 2], isDominant); %Aa
+phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [2 2], ~isDominant);%Aa
+phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [1 3], ~isDominant);%aa
+phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [2 3], isDominant);%aa
+
+
+
 % Replace the zeros in phentoypeFactor.val with the correct values.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
